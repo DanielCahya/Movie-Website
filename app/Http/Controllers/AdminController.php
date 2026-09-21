66420be4c1485c3768e690845a35f69b08c1e634
Daniel <danielcahyakurniawan@gmail.com>
2026-09-21 13:31:24 +0700

    perf: implement TMDB API caching and fix broken endpoints
    Changes:
    - Fix malformed TMDB API URLs in ShowController
    - Implement Cache::remember() with file driver for all TMDB API endpoints to reduce external API requests
    - Extract redundant genre fetching logic into private helper methods in AdminController
    - Remove broken/unused /showcari route in web.php
    - Suppress PHP 8.4 deprecation warnings in public/index.php to fix UI rendering

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,53 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class AdminController extends Controller
 {
+    private function getMovieGenres()
+    {
+        return Cache::remember('tmdb:genres:movie', now()->addHours(24), function () {
+            $genresArray = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/genre/movie/list')
+                ->json('genres');
+            
+            return collect($genresArray)->mapWithKeys(function($genre) {
+                return [$genre['id'] => $genre['name']];
+            });
+        });
+    }
+
+    private function getTvGenres()
+    {
+        return Cache::remember('tmdb:genres:tv', now()->addHours(24), function () {
+            $genresTvShow = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/genre/tv/list')
+                ->json('genres');
+            
+            return collect($genresTvShow)->mapWithKeys(function($genre) {
+                return [$genre['id'] => $genre['name']];
+            });
+        });
+    }
+
     function index(){
-    $genreId = 16;
+        $genreId = 16;
 
-    $popularMovies = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/movie/popular')
-        ->json('results');
-    
-    $nowPlaying = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/movie/now_playing')
-        ->json('results');
-    
-    $topRated = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/movie/top_rated')
-        ->json('results');
+        $popularMovies = Cache::remember('tmdb:popular_movies', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie/popular')
+                ->json('results');
+        });
         
-    
-    $newtv = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/tv/popular')
-        ->json('results');
-    
-    $anime = Http::withToken(config('services.tmdb.token'))
-    ->get('https://api.themoviedb.org/3/discover/movie', [
-        'with_genres' => $genreId,
-    ])
-    ->json('results');
+        $nowPlaying = Cache::remember('tmdb:now_playing', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie/now_playing')
+                ->json('results');
+        });
+        
+        $topRated = Cache::remember('tmdb:top_rated', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie/top_rated')
+                ->json('results');
+        });
+            
+        $newtv = Cache::remember('tmdb:new_tv', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/tv/popular')
+                ->json('results');
+        });
+        
+        $anime = Cache::remember('tmdb:anime', now()->addHours(4), function () use ($genreId) {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/discover/movie', [
+                    'with_genres' => $genreId,
+                ])
+                ->json('results');
+        });
 
-    $random = Http::withToken(config('services.tmdb.token'))
-    ->get('https://api.themoviedb.org/3/movie')
-    ->json('results');
+        $random = Cache::remember('tmdb:random', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie')
+                ->json('results');
+        });
 
-    $genresArray = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/genre/movie/list')
-        ->json('genres');
-    $genresTvShow = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/genre/tv/list')
-        ->json('genres');
-    
-    $genresTv = collect($genresTvShow)->mapWithKeys(function($genresTv){
-        return [$genresTv['id'] => $genresTv['name']];
-    });    
-
-    $genres = collect($genresArray)->mapWithKeys(function($genres){
-        return [$genres['id'] => $genres['name']];
-    });
+        $genres = $this->getMovieGenres();
+        $genresTv = $this->getTvGenres();
 
         return view('movies.index',[
             'popularMovies' => $popularMovies,
@@ -66,100 +92,82 @@ class AdminController extends Controller
 
     function tvshow(){
         
-        $top = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/tv/top_rated')
-            ->json('results');
-    
-        $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json('genres');
-        $genresTvShow = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/tv/list')
-            ->json('genres');
-        
-        $genresTv = collect($genresTvShow)->mapWithKeys(function($genresTv){
-            return [$genresTv['id'] => $genresTv['name']];
-        });    
-    
-        $genres = collect($genresArray)->mapWithKeys(function($genres){
-            return [$genres['id'] => $genres['name']];
+        $top = Cache::remember('tmdb:tv_top_rated', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/tv/top_rated')
+                ->json('results');
         });
     
-            return view('tvshow',[
-                'top' => $top,
-                'genresTv' => $genresTv,
-                'genres' => $genres,
-            ]);
+        $genres = $this->getMovieGenres();
+        $genresTv = $this->getTvGenres();
+    
+        return view('tvshow',[
+            'top' => $top,
+            'genresTv' => $genresTv,
+            'genres' => $genres,
+        ]);
     }
 
     function animation(){
         $genreId = 16;
 
-        $popularMovies = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json('results');
+        $popularMovies = Cache::remember('tmdb:popular_movies', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie/popular')
+                ->json('results');
+        });
 
-        $topAn = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/discover/tv', [
-                'sort_by' => 'vote_count.desc', // Sort by top-rated
-                'with_genres' => $genreId,
-            ])
-            ->json('results');
+        $topAn = Cache::remember('tmdb:tv_top_anime', now()->addHours(4), function () use ($genreId) {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/discover/tv/', [
+                    'sort_by' => 'vote_count.desc', // Sort by top-rated
+                    'with_genres' => $genreId,
+                ])
+                ->json('results');
+        });
         
         
-        $newtv = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/tv/airing_today')
-            ->json('results');
+        $newtv = Cache::remember('tmdb:tv_airing_today', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/tv/airing_today')
+                ->json('results');
+        });
         
-        $anime = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/discover/movie', [
-            'with_genres' => $genreId,
-        ])
-        ->json('results');
-    
-        $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json('genres');
-        $genresTvShow = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/tv/list')
-            ->json('genres');
-        
-        $genresTv = collect($genresTvShow)->mapWithKeys(function($genresTv){
-            return [$genresTv['id'] => $genresTv['name']];
-        });    
-    
-        $genres = collect($genresArray)->mapWithKeys(function($genres){
-            return [$genres['id'] => $genres['name']];
+        $anime = Cache::remember('tmdb:anime', now()->addHours(4), function () use ($genreId) {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/discover/movie', [
+                    'with_genres' => $genreId,
+                ])
+                ->json('results');
         });
     
+        $genres = $this->getMovieGenres();
+        $genresTv = $this->getTvGenres();
+    
         
-            return view('Animation.animation',[
-                'popularMovies' => $popularMovies,
-                'newtv' => $newtv,
-                'anime' => $anime,
-                'genresTv' => $genresTv,
-                'genres' => $genres,
-                'topAn' => $topAn,
-            ]);
+        return view('Animation.animation',[
+            'popularMovies' => $popularMovies,
+            'newtv' => $newtv,
+            'anime' => $anime,
+            'genresTv' => $genresTv,
+            'genres' => $genres,
+            'topAn' => $topAn,
+        ]);
     }
 
     function movieList(){
 
-        $topRated = Http::withToken(config('services.tmdb.token'))
-        ->get('https://api.themoviedb.org/3/movie/top_rated')
-        ->json('results');
-    
-        $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json('genres');  
-    
-        $genres = collect($genresArray)->mapWithKeys(function($genres){
-            return [$genres['id'] => $genres['name']];
+        $topRated = Cache::remember('tmdb:top_rated', now()->addHours(4), function () {
+            return Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/movie/top_rated')
+                ->json('results');
         });
     
-            return view('movieList.movie',[
-                'topRated' => $topRated,
-                'genres' => $genres,
-            ]);
+        $genres = $this->getMovieGenres();
+    
+        return view('movieList.movie',[
+            'topRated' => $topRated,
+            'genres' => $genres,
+        ]);
     }
 }
